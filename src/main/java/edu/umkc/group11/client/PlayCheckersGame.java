90c15700619022c7094e6675bf94c25355890d44
debsankar mukhopadhyay <dmf49@umkc.edu>
2021-10-11 14:36:16 -0500

    Adjusting the code

--- src/main/java/edu/umkc/group11/client/PlayCheckersGame.java
+++ src/main/java/edu/umkc/group11/client/PlayCheckersGame.java
@@ -8,23 +8,9 @@ import java.awt.*;
 
 public class PlayCheckersGame extends JFrame {
 
-    public PlayCheckersGame(String title)
+    private PlayCheckersGame(String title)
     {
         super(title);
-       /* setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout( new GridBagLayout() );
-        CheckerBoardUI tmpCheckerBoardUI = new CheckerBoardUI();
-        add(tmpCheckerBoardUI, new GridBagConstraints());
-        pack();
-        setVisible(true);
-        Box box = new Box(BoxLayout.Y_AXIS);
-        box.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-        box.add(Box.createVerticalGlue());
-        box.add(new CheckerBoardUI());
-        box.add(Box.createVerticalGlue());
-        add(box);
-        pack();
-        setVisible(true);*/
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel content = new JPanel(new GridBagLayout());
