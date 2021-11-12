a6cb3c3c9133ad1ebe4f68dae579cd1efffc9707
debsankar mukhopadhyay <dmf49@umkc.edu>
2021-11-12 12:18:13 -0600

    added unit tests

---
+++ src/test/java/edu/umkc/group11/GamePlayUtilTest.java
@@ -0,0 +1,29 @@
+package edu.umkc.group11;
+
+import edu.umkc.group11.client.GamePlayUtil;
+import edu.umkc.group11.model.BoardPanel;
+import edu.umkc.group11.model.PanelCoordinate;
+import edu.umkc.group11.model.Player;
+import edu.umkc.group11.screen.CheckerBoardUI;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class GamePlayUtilTest {
+
+    @Test
+    public void resetOtherPanelsAndButtonsTest()
+    {
+        CheckerBoardUI checkerBoardUI = new CheckerBoardUI(("Test1, Test2"));
+        BoardPanel boardPanel = checkerBoardUI.getBoardPanelByPanelCoordinate(new PanelCoordinate(2,1));
+        boardPanel.setPlayer(new Player(1, true));
+        boardPanel.getPlayer().setKing(true);
+        GamePlayUtil gamePlayUtil = new GamePlayUtil(checkerBoardUI);
+        boardPanel.getButton().setName("player1");
+
+        gamePlayUtil.resetOtherPanelsAndButtons(checkerBoardUI, new BoardPanel(checkerBoardUI,9).getButton());
+        String expexctedButtonLabel = "2,1 K ";
+        assertEquals(expexctedButtonLabel, boardPanel.getButton().getText());
+    }
+}
