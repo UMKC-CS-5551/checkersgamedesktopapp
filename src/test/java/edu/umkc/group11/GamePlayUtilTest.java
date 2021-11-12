1346131714a9e297ae27246b4c86b4fef99aa53a
debsankar mukhopadhyay <dmf49@umkc.edu>
2021-11-12 12:23:59 -0600

    added unit tests

--- src/test/java/edu/umkc/group11/GamePlayUtilTest.java
+++ src/test/java/edu/umkc/group11/GamePlayUtilTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class GamePlayUtilTest {
 
     @Test
-    public void resetOtherPanelsAndButtonsTest()
+    public void resetOtherPanelsAndButtonsTestKing()
     {
         CheckerBoardUI checkerBoardUI = new CheckerBoardUI(("Test1, Test2"));
         BoardPanel boardPanel = checkerBoardUI.getBoardPanelByPanelCoordinate(new PanelCoordinate(2,1));
@@ -26,4 +26,31 @@ public class GamePlayUtilTest {
         String expexctedButtonLabel = "2,1 K ";
         assertEquals(expexctedButtonLabel, boardPanel.getButton().getText());
     }
+    @Test
+    public void resetOtherPanelsAndButtonsTestNullPlayer()
+    {
+        CheckerBoardUI checkerBoardUI = new CheckerBoardUI(("Test1, Test2"));
+        BoardPanel boardPanel = checkerBoardUI.getBoardPanelByPanelCoordinate(new PanelCoordinate(2,1));
+        boardPanel.setPlayer(null);
+        GamePlayUtil gamePlayUtil = new GamePlayUtil(checkerBoardUI);
+
+        gamePlayUtil.resetOtherPanelsAndButtons(checkerBoardUI, new BoardPanel(checkerBoardUI,9).getButton());
+        String expexctedButtonLabel = "2,1";
+        assertEquals(expexctedButtonLabel, boardPanel.getButton().getText());
+    }
+
+    @Test
+    public void resetOtherPanelsAndButtonsTestPlayerId0()
+    {
+        CheckerBoardUI checkerBoardUI = new CheckerBoardUI(("Test1, Test2"));
+        BoardPanel boardPanel = checkerBoardUI.getBoardPanelByPanelCoordinate(new PanelCoordinate(2,1));
+        boardPanel.setPlayer(new Player(0, false));
+        GamePlayUtil gamePlayUtil = new GamePlayUtil(checkerBoardUI);
+        boardPanel.getButton().setName("player1");
+
+        gamePlayUtil.resetOtherPanelsAndButtons(checkerBoardUI, new BoardPanel(checkerBoardUI,9).getButton());
+        String expexctedButtonLabel = "2,1";
+        assertEquals(expexctedButtonLabel, boardPanel.getButton().getText());
+    }
+
 }
