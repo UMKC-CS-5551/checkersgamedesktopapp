56afbdce795f55a7d462127e8a7add300e356f9e
debsankar mukhopadhyay <dmf49@umkc.edu>
2021-11-13 00:50:51 -0600

    Update analytics.sh

--- analytics.sh
+++ analytics.sh
@@ -3,3 +3,4 @@ for ifile in $(git ls-files);
 		echo 'File Name: ' $ifile
 		for file in $(git ls-files $ifile); do git blame -c $file; done | tr '(' ' ' | awk '{print $2, $3}' | sort | uniq -c | sort -k1 -r;
 done
+ git ls-files | xargs wc -l
